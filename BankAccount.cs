ce357ae39980b53039b10b946f43428d0cfd9e2a
Skiddy Gamer <karels.jonass@gmail.com>
2020-05-28 17:01:21 +0300

    version 16:01

---
+++ BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalBank
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        public DateTime DateCreated { get; set; }
+
+        public BankAccount(string ownerName, decimal initialBalance)
+        {
+            this.DateCreated = DateTime.Now;
+            this.Number = "123";
+            this.Owner = ownerName;
+            this.Balance = initialBalance;
+        }
+
+        public void DepositMoney(decimal amount, DateTime date, string notes)
+        {
+
+        }
+            
+        public void WithdrawMoney(decimal amount, DateTime date, string notes)
+        {
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace DigitalBank
     {
         static void Main(string[] args)
         {
-            
+            BankAccount account = new BankAccount("Jenifer", 100);
+            Console.WriteLine($"Owner {account.Owner} has {account.Balance} euro(s) in account Nr. {account.Number} created on {account.DateCreated.ToString("dd/MM/yyyy")}.");
+            account.DateCreated = new DateTime(2020, 03, 01);
+            Console.WriteLine($"Owner {account.Owner} has {account.Balance} euro(s) in account Nr. {account.Number} created on {account.DateCreated.ToString("dd/MM/yyyy")}.");
         }
     }
 }
